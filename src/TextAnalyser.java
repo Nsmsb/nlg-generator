583917182d553d6d0a0ea34592baeec100fbfd38
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2020-02-07 16:28:34 +0100

    add: methodes, fix: bugs

--- src/TextAnalyser.java
+++ src/TextAnalyser.java
@@ -1,4 +1,7 @@
 import java.io.FileInputStream;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Hashtable;
 import java.util.Scanner;
 
@@ -6,10 +9,10 @@ import javafx.util.Pair;
 
 class TextAnalyser {
 
-    Hashtable<String,WordPair> wordsFreqs;
-    String f1; // the learning file
-    Scanner in1; // the scanner associated to the file
-    int words;
+    private Hashtable<String,WordPair> wordsFreqs;
+    private String f1; // the learning file
+    private Scanner in1; // the scanner associated to the file
+    private int words;
 
 
     public TextAnalyser(String f1) {
@@ -20,14 +23,24 @@ class TextAnalyser {
         catch (Exception e) {System.out.println(e);}
     }
 
-    void analyse() {
+    public int getwords() {
+        return this.words;
+    }
+
+    public int getWerdFrequency(String word) {
+        if (this.wordsFreqs.contains(word))
+            return (wordsFreqs.get(word).getValue() / this.words);
+        return 0;
+    }
+
+    public void analyse() {
         String[] words;
         try {
             while (in1.hasNext()) {
                 words = in1.next().split("\\W+");
 
                 for (String word : words) {
-                    if (wordsFreqs.contains(word)) {
+                    if (wordsFreqs.containsKey(word)) {
                         wordsFreqs.get(word).updateValue();
                         this.words++;
                     }
@@ -38,6 +51,20 @@ class TextAnalyser {
         } catch (Exception e) {
             System.out.println(e);
         }
+    }
+
+    public WordPair[] getNthFreqWords(int n) {
+        WordPair[] result = new WordPair[this.wordsFreqs.size()];
+        result = this.wordsFreqs.values().toArray(result);
+        System.out.println("hhhhh");
+        Arrays.sort(result, new Comparator<WordPair>() {
+            public int compare(WordPair w1, WordPair w2) {
+                return w2.getValue() - w1.getValue();
+            }
+        });
+
+        return Arrays.copyOf(result, n);
+
     }
 
 
